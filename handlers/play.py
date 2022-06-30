181f2018560f671a0a2456b01aa734d02f0ecd73
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-07-01 01:00:18 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -1,3 +1,4 @@
+import io
 import os
 from os import path
 from pyrogram import Client, filters
@@ -21,7 +22,43 @@ import ffmpeg
 from pytgcalls import StreamType
 from pytgcalls.types.input_stream import InputAudioStream
 from pytgcalls.types.input_stream import InputStream
+from PIL import Image, ImageDraw, ImageFont
 
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
+
+
+async def generate_cover(thumbnail, title, userid, ctitle):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(f"thumb{userid}.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    image1 = Image.open(f"thumb{userid}.png")
+    images = choice(ZAID_IMGS)
+    image2 = Image.open(images)
+    image3 = changeImageSize(1280, 720, image1)
+    image4 = changeImageSize(1280, 720, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save(f"temp{userid}.png")
+    img = Image.open(f"temp{userid}.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("Process/ImageFont/finalfont.ttf", 60)
+    font2 = ImageFont.truetype("Process/ImageFont/finalfont.ttf", 70)     
+    draw.text((20, 45), f"{title[:30]}...", fill= "white", stroke_width = 1, stroke_fill="white", font=font2)
+    draw.text((120, 595), f"PlAYING ON: {ctitle[:20]}...", fill="white", stroke_width = 1, stroke_fill="white" ,font=font)
+    img.save(f"final{userid}.png")
+    os.remove(f"temp{userid}.png")
+    os.remove(f"thumb{userid}.png") 
+    final = f"final{userid}.png"
+    return final
 
 def transcode(filename):
     ffmpeg.input(filename).output("input.raw", format='s16le', acodec='pcm_s16le', ac=2, ar='48k').overwrite_output().run() 
